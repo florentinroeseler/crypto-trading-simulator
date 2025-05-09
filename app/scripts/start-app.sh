ba4a722719a166ab8b0f863d162b0d8832c6cc6d
knusperreis23 <knusperreis23@gmail.com>
2025-05-09 23:11:37 +0200

    besser

--- app/scripts/start-app.sh
+++ app/scripts/start-app.sh
@@ -6,6 +6,12 @@ echo "Installiere Abhängigkeiten..."
 cd /app
 npm install
 
+echo "Führe Datenbank-Migration aus..."
+npm run db:migrate
+
+echo "Führe Datenbank-Seed aus..."
+npm run db:seed
+
 # Starte die Anwendung ohne Migrationen
 echo "Starte SvelteKit-Anwendung..."
 npm run dev -- --host
